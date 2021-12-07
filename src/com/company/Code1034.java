458038a8ed9a998dc0a0b4fadf012d4fd5caa844
weizi-a <1345014465@qq.com>
2021-12-07 22:54:55 +0800

    1034 leetcode

---
+++ src/com/company/Code1034.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * @program: code
+ * @description:
+ * @author:
+ * @create:
+ **/
+public class Code1034 {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[2][2];
+        arr[0][0] = 1;
+        arr[0][1] = 1;
+        arr[1][0] = 1;
+        arr[1][1] = 2;
+        System.out.println(arr.length);
+//        for (int i = 0,j=0; i < arr.length&&j<; i++) {
+//
+//        }
+
+    }
+
+
+}
